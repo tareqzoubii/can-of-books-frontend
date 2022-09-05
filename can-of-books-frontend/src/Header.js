469bed888ec23f81322884b0cf0a6c3e67c28a52
tareqzoubii <tareqzoubi4@gmail.com>
2022-09-04 22:43:19 -0700

    V1

---
+++ can-of-books-frontend/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1 style={{textAlign:"center"}}>Welcome to Books App</h1>
+            </div>
+        )
+    }
+}
+export default Header;
